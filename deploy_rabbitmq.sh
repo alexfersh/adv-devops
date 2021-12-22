a2b79105dbf445236b18d4cef816a3a4310b2c4a
alexfersh <alex.fershtein@gmail.com>
2021-12-23 01:22:27 +0200

    Making final tuning to project configuration

--- deploy_rabbitmq.sh
+++ deploy_rabbitmq.sh
@@ -10,15 +10,19 @@ helm repo update
 namespace=$(cat ./helm/values.yaml | grep -w namespace: | tr '\t\n' ' ' | tr -s ' ' | cut -d ' ' -f2)
 echo "Namespace:" $namespace
 releasename=$(cat ./helm/values.yaml | grep -w releasename: | tr '\t\n' ' ' | tr -s ' ' | cut -d ' ' -f2)
-echo "Release Name:" $releasename
+echo "Helm release:" $releasename
 
 check_namespace=$(kubectl get namespace | grep -w $namespace | tr '\t\n' ' ' | tr -s ' ' | cut -d ' ' -f1)
+echo "Namespace to check:" $check_namespace
 if [ $check_namespace != $namespace ]; then
-	kubectl create $namespace
+	kubectl create namespace $namespace
 fi
 
 check_release=$(helm --namespace=$namespace list | grep -w $releasename | tr '\t\n' ' ' | tr -s ' ' | cut -d ' ' -f1)
+echo "Helm release to check:" $check_release
 if [ $check_release != $releasename ]; then
+
+        echo "RabbitMQ Helm chart is not installed yet. Installing..."
 
 	helm upgrade $releasename $RABBITMQ_REGISTRY -f rabbitmq-values.yaml --install --force --namespace=$namespace --create-namespace
 
@@ -34,12 +38,14 @@ if [ $check_release != $releasename ]; then
 	sed -i "s/('guest', 'guest')/('$rabbitmq_username', '$rabbitmq_password')/" ./producer/producer.py
 
 else
+        echo "RabbitMQ installation was found. Upgrading release..."
+
 	podname=`kubectl --namespace $namespace get pods | grep -w rabbitmq-0 | cut -d ' ' -f1`
 	rabbitmq_secret=`kubectl --namespace $namespace describe pods $podname | grep -w RABBITMQ_PASSWORD: | tr '\t\n' ' ' | tr -s ' ' | tr -d "<'>" | cut -d ':' -f2 | cut -d ' ' -f9`
 	RABBITMQ_PASSWORD=$(kubectl --namespace $namespace get secret $rabbitmq_secret -o jsonpath="{.data.rabbitmq-password}" | base64 -d)
 	RABBITMQ_ERLANG_COOKIE=$(kubectl --namespace $namespace get secret $rabbitmq_secret -o jsonpath="{.data.rabbitmq-erlang-cookie}" | base64 -d)
 
-	helm upgrade $releasename $RABBITMQ_REGISTRY -f rabbitmq-values.yaml --install --force --namespace=$namespace --set auth.password=$RABBITMQ_PASSWORD --set auth.erlangCookie=$RABBITMQ_ERLANG_COOKIE
+	helm upgrade $releasename $RABBITMQ_REGISTRY -f rabbitmq-values.yaml --install --force --namespace=$namespace --create-namespace --set auth.password=$RABBITMQ_PASSWORD --set auth.erlangCookie=$RABBITMQ_ERLANG_COOKIE
 
 	cluster_ip=`kubectl --namespace $namespace get svc | grep -w $releasename | grep -v headless | tr '\t\n' ' ' | tr -s ' ' | cut -d ' ' -f3`
 	podname=`kubectl --namespace $namespace get pods | grep -w rabbitmq-0 | cut -d ' ' -f1`
